ce93b9c4d1c0ff511022d666af0af87ec7587079
Si <tlgyrjs@naver.com>
2024-09-30 12:51:29 +0900

    Spring core 모듈 의존성 관리 학습중(chap 03)

---
+++ src/test/java/com/study/springstudy/core/chap03/HotelTest.java
@@ -0,0 +1,12 @@
+package com.study.springstudy.core.chap03;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HotelTest {
+
+    @Test
+    void inform() {
+    }
+}
